e27f39392e0660477c95da7baaefb32c89b882c4
Jiyong Jung <jiyongjung@google.com>
2020-08-03 17:22:39 +0900

    Update base_stub_executor.py

--- tfx/experimental/pipeline_testing/base_stub_executor.py
+++ tfx/experimental/pipeline_testing/base_stub_executor.py
@@ -14,10 +14,6 @@
 # limitations under the License.
 """Base Stub Executor."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import os
 from typing import Any, Dict, List, Text, Optional
 
